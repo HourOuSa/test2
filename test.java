e724f0293c1e8f40b6f6bd5178220e826b79f14b
VuthMenghuor <hourousa9@gmail.com>
2024-03-19 16:36:17 +0700

    hello

---
+++ test.java
@@ -0,0 +1,10 @@
+package test2;
+
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello bro vuth menghuor");
+        System.out.println("Welcome to Java");
+    }
+}
+
